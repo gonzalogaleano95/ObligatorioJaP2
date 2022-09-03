7577d7eef1bb643d52de25df79aef052d2270474
gonzalogaleano95 <galeano.g95@gmail.com>
2022-09-03 18:53:43 -0300

    comentarios
    js de products y login

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
-document.getElementById("boton").addEventListener("click", ()=>{
-
-        let mail = document.getElementById("mailLog").value;
-        let contra = document.getElementById("contraLog").value;
+document.getElementById("boton").addEventListener("click", ()=>{                //encuentro el elemento con Id boton y le añado un event listener para que, en el evento de un click
+                                                                                //del botón para logearse, se creen las variables mail y contra. El valor de estas variables va a ser igual
+        let mail = document.getElementById("mailLog").value;                    //al valor introducido en los elementos con Id mailLog y contraLog respectivamente. Luego, se revisa que
+        let contra = document.getElementById("contraLog").value;                //esos valores no estén vacíos. Si no lo están, se redirecciona a la página principal.
     
         if((mail != "") && (contra != "")){
             window.location = "pagina_principal.html";
--- js/products.js
+++ js/products.js
@@ -1,15 +1,15 @@
-async function array(){
-    let response = await fetch(`${PRODUCTS_URL}/101${EXT_TYPE}`);
-    let data = await response.json();
-    console.log(data.products);
-    LoadDescription(data.products)
+async function array(){                                                 //creo función asíncrona llamada array
+    let response = await fetch(`${PRODUCTS_URL}/101${EXT_TYPE}`);       //declaro la variable response, la cual va a esperar el fetch
+    let data = await response.json();                                   //declaro la variiable data, en la cual se esperará el valor de response en formato .json
+    console.log(data.products);                                         //muestro los productos en la consola para llevar un control
+    LoadData(data.products)                                      //ejecuta la función cargará los datos de los productos
 }
-array();
+array();                                                                //muestro la función
 
-function LoadDescription(data){
-    for(const item of data){
-        document.getElementById("autos").innerHTML += 
-        `
+function LoadData(data){                                         //defino la función que me va a cargar los datos del fetch
+    for(const item of data){                                            //recorro cada item dentro de data
+        document.getElementById("autos").innerHTML +=                   //encuentro el elemento con el Id autos y le adjunto un grupo de elementos html, en los cuales se encontrarán los datos requeridos en display
+        `                                                               
         <img src="${item.image}"/>
         <h2>${item.name}</h2> 
         <h3>${item.currency} ${item.cost}</h3>
